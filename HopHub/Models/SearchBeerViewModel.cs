565870b7adb4c0266c3c752121d76e1ebd3de28d
Nicholas Montanino <montanino@utexas.edu>
2017-10-18 19:48:20 -0400

    Add search bar to index.

---
+++ HopHub/Models/SearchBeerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HopHub.Models
+{
+    public class SearchBeerViewModel
+    {
+        [Required]
+        [Display(Name = "Enter search term:")]
+        public string Query { get; set; }
+
+        // TODO: Include list of Beer objects to be returned during search
+    }
+}

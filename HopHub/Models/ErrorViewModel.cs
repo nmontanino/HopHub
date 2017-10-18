34ff436793b58ae4cac8bfbd5e215eb43e6a894d
Nicholas Montanino <montanino@utexas.edu>
2017-10-18 14:09:04 -0400

    Add project files.

---
+++ HopHub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HopHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

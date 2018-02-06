4cad5649c2cc6e5347612477057dd1ddda29abc5
Nicholas Montanino <montanino@utexas.edu>
2018-02-06 15:40:53 -0500

    Fix bug where next page button displays on last page.

--- HopHub/wwwroot/js/search.js
+++ HopHub/wwwroot/js/search.js
@@ -11,12 +11,14 @@ function getBeers(query, pageNum) {
             let pages = response.numberOfPages;
             let currentPage = response.currentPage;
 
-            // Create pager if more than 50 results
+            // Create pager component if returning more than 50 results
             if (pages > 1) {
                 if (currentPage > 1) {
                     $('.pager').append(`<li class="previous"><a href="?beer=${query}&pageNum=${currentPage - 1}">&larr; Previous</a></li>`);
                 }
-                $('.pager').append(`<li class="next"><a href="?beer=${query}&pageNum=${currentPage + 1}">Next &rarr;</a></li>`);
+                if (currentPage !== pages) {
+                    $('.pager').append(`<li class="next"><a href="?beer=${query}&pageNum=${currentPage + 1}">Next &rarr;</a></li>`);
+                }
             }
 
             // Display number of results and page number
@@ -27,22 +29,37 @@ function getBeers(query, pageNum) {
                 $('.results').append("<i>No search results.</i>");
             }
 
-            for (let i = 0; i < beers.length; i++) {
+            let resultsOnPage = beers.length;
+            for (let i = 0; i < resultsOnPage; i++) {
 
                 let name = beers[i].name;
-                let style = beers[i].style;
+                let style = (beers[i].style);
                 let abv = beers[i].abv;
                 let description = beers[i].description;
                 let beerId = beers[i].id;
                 let brewery = beers[i].breweries[0];
-
-                // Use placeholder image if label not available
                 let image = beers[i].labels;
 
+                /*
+                Checking for undefined values using ternary operators inside template strings. 
+
+                let markup = `
+                    <div class="well">
+                        ${image ? `<div class="pull-right"><img src="${image.icon}"></div>` : `<div class="pull-right"><img class="thumbnail" src="/images/noun_3235_cc-gry2-lg.svg" height="64px"></div>`}
+                        <b><a href="/Beer?id=${beerId}">${name}</a></b><br>
+                        ${style ? `<b>${style.name}</b><br>` : ''}
+                        ${brewery ? `<b>${brewery.name}</b><br>` : ''}
+                        ${abv ? `<b>ABV: ${abv}%</b><br>` : ''}
+                    </div>
+                `;
+                $('.results').append(markup);
+                */
+
+                // Use placeholder image if label not available
                 if (notEmpty(image)) {
-                    $('.results').append(`<div class="pull-right"><img src="${image.icon}"><div>`);
+                    $('.results').append(`<div class="pull-right"><img src="${image.icon}"></div>`);
                 } else {
-                    $('.results').append(`<div class="pull-right"><img class="thumbnail" src="/images/noun_3235_cc-gry2-lg.svg" height="64px"><div>`);
+                    $('.results').append(`<div class="pull-right"><img class="thumbnail" src="/images/noun_3235_cc-gry2-lg.svg" height="64px"></div>`);
                 }
 
                 // Link to individual beer page
